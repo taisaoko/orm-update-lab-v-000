78400fd533099741a909cc6951b4413da58f2cd8
taisaoko <ttd3calvin@gmail.com>
2018-12-11 23:56:37 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -42,8 +42,8 @@ class Student
     end
   end
   
-  def self.create(name, grade)
-    student = Student.new(name, grade)
+  def self.create(name:, grade:)
+    student = Student.new(name:, grade:)
     student.save
     student
   end
