2bd61c8cbe4edb000a201579ecbffe677aba067b
taisaoko <ttd3calvin@gmail.com>
2018-12-11 23:55:16 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -42,17 +42,14 @@ class Student
     end
   end
   
-  def self.create(name:, grade:)
+  def self.create(name, grade)
     student = Student.new(name, grade)
     student.save
     student
   end
 
   def self.new_from_db(row)
-      new_student = self.new  # self.new is the same as running Student.new
-      new_student.id = row[0]
-      new_student.name =  row[1]
-      new_student.grade = row[2]
+      new_student = self.new(row[0], row[1], row[2])  # self.new is the same as running Student.new
       new_student  # return the newly created instance
       # create a new Student object given a row from the database
   end
